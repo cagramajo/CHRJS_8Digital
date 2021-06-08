2d8226805292de1642a3dabf37b1763b05b8f996
Eduardo Gramajo <echarly02@gmail.com>
2021-06-08 19:55:24 -0300

    Entrega Clase 3 - preliminar

---
+++ src/component/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class NavBar extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Mi eQR market</h1>
+                <ul>
+                    <li>Electronica</li>
+                    <li>Cursos</li>
+                    <li>e-Book</li>
+                </ul>
+            </div>
+        )
+    }
+}
