18285c71aa5deb7a36fc1f8ed44ad462fc0e4cab
Eduardo Gramajo <echarly02@gmail.com>
2021-06-10 09:37:14 -0300

    Correccion por elementos llamados sin contenido

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import NavBar from './component/NavBar/NavBar';
-import ItemListContainer from './component/ItemListContainer/ItemListContainer'
+//import ItemListContainer from './component/ItemListContainer/ItemListContainer';
 // Componentes
 
 
@@ -8,7 +8,6 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      <ItemListContainer/>
     </div>
   );
 }
--- src/component/NavBar/NavBar.js
+++ src/component/NavBar/NavBar.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
-import CartWidget from "../CartWidget/CartWidget";
+//mport CartWidget from "../CartWidget/CartWidget";
+import './NavBar.css';
 
 export default class NavBar extends Component {
     render() {
         return (
-            <div>
+            <div className='NavBar'>
                 <h1>Mi eQR market</h1>
                 <ul>
                     <li>Electronica</li>
                     <li>Cursos</li>
                     <li>e-Book</li>
                 </ul>
-                <CartWidget />
             </div>
         )
     }
