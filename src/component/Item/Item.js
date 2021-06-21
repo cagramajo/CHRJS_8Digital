33c7f519f9be1accb4e0af70d8e1161f2735f9d6
Eduardo <echarly02@gmail.com>
2021-06-20 23:19:52 -0300

    Carga de items usando useEffect y Promise

--- src/component/Item/Item.js
+++ src/component/Item/Item.js
@@ -2,16 +2,16 @@ import React from "react";
 
 export default function Item({Photo, AltPhoto, NameProduct, LittleDesciption, Price}) {
   return (
-    <div class="ui card">
-      <div class="image">
+    <div className="ui card">
+      <div className="image">
         <img src={Photo} alt={AltPhoto}/>
       </div>
-      <div class="content">
-        <div class="header">{NameProduct}</div>
-        <div class="description">{LittleDesciption}</div>
+      <div className="content">
+        <div className="header">{NameProduct}</div>
+        <div className="description">{LittleDesciption}</div>
       </div>
-      <div class="extra content">
-        <i aria-hidden="true" class="usd icon"></i>{Price}
+      <div className="extra content">
+        <i aria-hidden="true" className="usd icon"></i>{Price}
       </div>
     </div>
   );
--- src/component/ItemList/ItemList.js
+++ src/component/ItemList/ItemList.js
@@ -1,32 +1,67 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Item from "../Item/Item";
 
 export default function ItemList() {
+
+
+    const [productList, setProductList] = useState([])
+
+    useEffect(() => {
+        const loadPromise = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                console.log("Pasaron 20 Seg!");
+                setProductList(loadItem);
+            }, 3000);
+            console.log("Estamos aquí");
+        })
+//        loadPromise.then((resolve)=>{
+//            console.log(resolve);
+//            setProductList(loadItem);
+//        })
+    }, [])
+
+    const loadItem = () => {
+        let pList = [
+            {
+                IdProduct:1,
+                UrlPhoto:"https://www.cannaconnection.es/12098/og-kush.jpg",
+                AltPhoto:"Flower",
+                NameProduct:"OG Kush",
+                LittleDesciption:"Un clasico popular por buenas razones.",
+                Price:"2200"
+            },
+            {
+                IdProduct:2,
+                UrlPhoto:"https://www.cannaconnection.es/12098/og-kush.jpg",
+                AltPhoto:"Flower",
+                NameProduct:"OG Kush",
+                LittleDesciption:"Un clasico popular por buenas razones.",
+                Price:"2300"
+            },
+            {
+                IdProduct:3,
+                UrlPhoto:"https://www.cannaconnection.es/12098/og-kush.jpg",
+                AltPhoto:"Flower",
+                NameProduct:"OG Kush",
+                LittleDesciption:"Un clasico popular por buenas razones.",
+                Price:"2400"
+            }
+        ]
+        return pList;
+    }
+
   return (
     <div >
-      <Item
-        Photo="https://www.cannaconnection.es/12098/og-kush.jpg"
-        AltPhoto="Flower"
-        NameProduct="OG Kush"
-        LittleDesciption="Un clasico popular por buenas razones."
-        Price="2200"
-      />
-
-      <Item
-        Photo="https://www.cannaconnection.es/12098/og-kush.jpg"
-        AltPhoto="Flower"
-        NameProduct="OG Kush"
-        LittleDesciption="Un clasico popular por buenas razones."
-        Price="2200"
-      />
-
-      <Item
-        Photo="https://www.cannaconnection.es/12098/og-kush.jpg"
-        AltPhoto="Flower"
-        NameProduct="OG Kush"
-        LittleDesciption="Un clasico popular por buenas razones."
-        Price="2200"
-      />
+        {productList.map(item =>
+            <Item
+                key={item.IdProduct}
+                Photo={item.UrlPhoto}
+                AltPhoto={item.AltPhoto}
+                NameProduct={item.NameProduct}
+                LittleDesciption={item.LittleDesciption}
+                Price={item.Price}
+            />
+            )}
     </div>
   );
 }
